c2f5a1d058748a6613b6c48918ced35aaee7f8e1
Matthias Meidinger <mmeiding@redhat.com>
2022-02-21 16:58:39 +0100

    Introduce SPDX ID script

---
+++ tools/apply-spdx-ids.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import argparse
+from asyncore import write
+import pathlib
+
+# EXCLUDED_EXTENSIONS = ["pb.go", "pb.gw.go"]
+
+FILE_HEADER = """// Copyright StackRox Authors
+// SPDX-License-Identifier: Apache-2.0
+
+"""
+
+
+def find_files(path, fileglob):
+    files_full = list(path.glob(fileglob))
+    return files_full
+
+
+def contains_header(filepath):
+    with open(filepath, mode="r") as file:
+        # Check for the content of the first comment line
+        print(f"checking for {FILE_HEADER.splitlines()[0].split('//')[1].strip()}")
+        if FILE_HEADER in file.read():
+            print("found in file")
+            return True
+        return False
+
+
+def write_header(filepath):
+    with open(filepath, "r+") as file:
+        print(f"opening file {filepath}")
+        content = file.read()
+        file.seek(0, 0)
+        file.write(FILE_HEADER + content)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=pathlib.Path, help="Path to project basedir")
+    arguments = parser.parse_args()
+
+    source_files = find_files(arguments.path, "**/*[!pb|!pb.gw].go")
+    proto_files = find_files(arguments.path, "**/?*.proto")
+
+    for file in proto_files+source_files:
+        if not contains_header(file):
+            write_header(file)
+
+
+
+if __name__ == "__main__":
+    main()
