43a2a479e6250ead66ad18d27ccdd2021a4c827e
David Vail <dcvail17@gmail.com>
2022-07-13 10:45:01 -0400

    Implement dark theme for PatternFly.

--- ui/apps/platform/src/index.tsx
+++ ui/apps/platform/src/index.tsx
@@ -25,6 +25,10 @@ import '@patternfly/react-styles/css/utilities/Sizing/sizing.css';
 import '@patternfly/react-styles/css/utilities/Spacing/spacing.css';
 import '@patternfly/react-styles/css/utilities/Text/text.css';
 
+// The below line and the `@patternfly/patternfly` dependency can be removed if and when
+// the following import is merged into the main react-charts CSS.
+import '@patternfly/patternfly/patternfly-charts-theme-dark.css';
+
 // Advanced Cluster Security extensions to PatternFly styles
 import 'css/acs.css';
 // We need the following file, to smooth out rough edges, as we migrate to PatternFly
