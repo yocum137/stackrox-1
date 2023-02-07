dcd0f4ef2f0e088d841c8937bc467ea146ca65ae
Matthias Meidinger <mmeiding@redhat.com>
2023-02-07 18:11:03 +0100

    WIP: Rename env var, introduce backoff

--- pkg/env/node_scan.go
+++ pkg/env/node_scan.go
@@ -7,5 +7,5 @@ var (
 	NodeRescanInterval = registerDurationSetting("ROX_NODE_RESCAN_INTERVAL", 4*time.Hour)
 
 	// NodeInventoryCacheDuration defines the time after which a cached inventory is considered outdated
-	NodeInventoryCacheDuration = registerDurationSetting("ROX_NODE_INVENTORY_CACHE", 10*time.Minute)
+	NodeInventoryCacheDuration = registerDurationSetting("ROX_NODE_INVENTORY_CACHE_TIME", 10*time.Minute)
 )
