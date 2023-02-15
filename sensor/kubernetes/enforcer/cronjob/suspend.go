9bdc76a0e18b85b4439cfce1c48844015520b0d5
Khushboo Sancheti <ks@stackrox.com>
2023-02-14 21:24:54 -0800

    ROX-15113: Add support to suspend a CrobJob while enforcing a deploy time policy

---
+++ sensor/kubernetes/enforcer/cronjob/suspend.go
@@ -0,0 +1,62 @@
+package cronjob
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/stackrox/rox/generated/internalapi/central"
+	"github.com/stackrox/rox/pkg/branding"
+	kubernetesPkg "github.com/stackrox/rox/pkg/kubernetes"
+	"github.com/stackrox/rox/pkg/retry"
+	"github.com/stackrox/rox/sensor/utils"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	batchV1      = "batch/v1"
+	batchBeta1v1 = "batch/beta1v1"
+)
+
+// Suspend suspends the cron job
+func Suspend(ctx context.Context, client kubernetes.Interface, deploymentInfo *central.DeploymentEnforcement) (err error) {
+	depName := deploymentInfo.GetDeploymentName()
+	depType := deploymentInfo.GetDeploymentType()
+	forcePatch := true
+
+	if ok, apiErr := utils.HasAPI(client, batchV1, kubernetesPkg.CronJob); ok && apiErr == nil {
+		patch := fmt.Sprintf("{\"metadata\": {\"name\": %s}, \"kind\": %s, \"apiVersion\": %s, \"spec\": {\"suspend\": true}}", depName, depType, batchV1)
+
+		_, err = client.BatchV1().CronJobs(deploymentInfo.GetNamespace()).Patch(ctx, depName, types.ApplyPatchType,
+			[]byte(patch),
+			metav1.PatchOptions{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       depType,
+					APIVersion: batchV1,
+				},
+				FieldManager: branding.GetProductName(),
+				Force:        &forcePatch,
+			})
+		if err != nil {
+			return retry.MakeRetryable(err)
+		}
+	} else {
+		patch := fmt.Sprintf("{\"metadata\": {\"name\": %s}, \"kind\": %s, \"apiVersion\": %s, \"spec\": {\"suspend\": true}}", depName, depType, batchBeta1v1)
+
+		_, err = client.BatchV1beta1().CronJobs(deploymentInfo.GetNamespace()).Patch(ctx, depName, types.ApplyPatchType,
+			[]byte(patch),
+			metav1.PatchOptions{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       depType,
+					APIVersion: batchBeta1v1,
+				},
+				FieldManager: branding.GetProductName(),
+				Force:        &forcePatch,
+			})
+		if err != nil {
+			return retry.MakeRetryable(err)
+		}
+	}
+	return nil
+}
