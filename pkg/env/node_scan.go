dab6b5687d05a453fa71082e0472206dedd61d35
Matthias Meidinger <mmeiding@redhat.com>
2023-02-20 12:05:49 +0100

    Review comments part 1
    - Update docstring for env vars
    - Update log messages
    - Rename backoffFile var
    - Rename backoff filename

--- pkg/env/node_scan.go
+++ pkg/env/node_scan.go
@@ -3,7 +3,7 @@ package env
 import "time"
 
 var (
-	// NodeRescanInterval will set the duration for when to fetch node inventory to be scanned for vulnerabilities
+	// NodeRescanInterval sets the time between periodic node rescans for changed components. Results are cached in case they need to be resubmitted for NodeInventoryCacheDuration.
 	NodeRescanInterval = registerDurationSetting("ROX_NODE_RESCAN_INTERVAL", 4*time.Hour)
 
 	// NodeInventoryCacheDuration defines the time after which a cached inventory is considered outdated. Defaults to 90% of rescan interval.
